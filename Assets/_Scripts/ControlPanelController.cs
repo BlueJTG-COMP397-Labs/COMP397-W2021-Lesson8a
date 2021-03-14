811844cc8bbd3cb463b34e26be3c2788782c4ee7
Timothy Garcia <timothyiangarcia@gmail.com>
2021-03-13 20:20:32 -0500

    added pause and minimap buttons

--- Assets/_Scripts/ControlPanelController.cs
+++ Assets/_Scripts/ControlPanelController.cs
@@ -10,7 +10,7 @@ public class ControlPanelController : MonoBehaviour
     public Vector2 offScreenPosition;
     public Vector2 onScreenPosition;
 
-    [Range(0.1f, 10.0f)] 
+    [Range(0.1f, 10.0f)]
     public float speed = 1.0f;
     public float timer = 0.0f;
     public bool isOnScreen = false;
@@ -32,23 +32,10 @@ public class ControlPanelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            isOnScreen = !isOnScreen;
-            timer = 0.0f;
-
-            if (isOnScreen)
-            {
-                Cursor.lockState = CursorLockMode.None;
-                playerCamera.enabled = false;
-            }
-            else
-            {
-                
-                Cursor.lockState = CursorLockMode.Locked;
-                playerCamera.enabled = true;
-            }
-        }
+        //if (Input.GetKeyDown(KeyCode.Tab))
+        //{
+        //    ToggleControlPanel();
+        //}
 
         if (isOnScreen)
         {
@@ -59,6 +46,24 @@ public class ControlPanelController : MonoBehaviour
             MoveControlPanelUp();
         }
     }
+
+    void ToggleControlPanel()
+    {
+        isOnScreen = !isOnScreen;
+        timer = 0.0f;
+        if (isOnScreen)
+        {
+            //Cursor.lockState = CursorLockMode.None;
+            playerCamera.enabled = false;
+        }
+        else
+        {
+
+            //Cursor.lockState = CursorLockMode.Locked;
+            playerCamera.enabled = true;
+        }
+    }
+
 
     private void MoveControlPanelDown()
     {
@@ -81,5 +86,10 @@ public class ControlPanelController : MonoBehaviour
         {
            pausable.TogglePause();
         }
+    }
+
+    public void OnControlButtonPressed()
+    {
+        ToggleControlPanel();
     }
 }
